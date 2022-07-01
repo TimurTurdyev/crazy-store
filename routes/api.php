bf17ee872154a5f88b23f4fdb10798dd834116a2
Timur <borodatimur@gmail.com>
2022-07-01 09:37:31 +0300

    route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+Route::post('cdek-api/{method}', [\App\Http\Controllers\Api\CdekController::class, 'index']);
+
